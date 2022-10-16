a45b98e6f897b9ae13b12686778ce4c0fa68f5c6
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2022-10-16 17:30:29 +0530

    Tailwind css and global css setup (#1)
    chore : tailwind css and global css setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>Innovision 2022</div>;
+  return <h1 className='text-3xl font-bold underline text-white'>Innovision 2022</h1>;
 }
