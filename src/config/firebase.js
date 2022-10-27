cf7ef4e1b13bb0c52454098359190d52c3ead156
Hemant Bajaj <41hemu38@gmail.com>
2022-10-27 22:43:21 +0530

    feat: setup env for firebase credentials

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDZnqFdSVjs_n7cmq5vUxW9D44xq23Dg-s',
-  authDomain: 'inno-demo-c9930.firebaseapp.com',
-  projectId: 'inno-demo-c9930',
-  storageBucket: 'inno-demo-c9930.appspot.com',
-  messagingSenderId: '769561537856',
-  appId: '1:769561537856:web:7cb3be5a84ed5ca917dea5',
-  measurementId: 'G-JND7HMC7HL',
+  apiKey: process.env.GATSBY_Api_Key,
+  authDomain: process.env.GATSBY_Auth_Domain,
+  projectId: process.env.GATSBY_Project_Id,
+  storageBucket: process.env.GATSBY_Storage_Bucket,
+  messagingSenderId: process.env.GATSBY_Messaging_Sender_Id,
+  appId: process.env.GATSBY_App_Id,
+  measurementId: process.env.GATSBY_Measurement_Id,
 };
 
 // Initialize Firebase
