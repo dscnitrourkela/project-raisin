47d7a3aeeccc9547ec39d54c102763ad7448d5e5
Hemant Bajaj <41hemu38@gmail.com>
2022-11-02 12:15:17 +0530

    feat: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,19 @@ module.exports = {
         // Add any options here
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GATSBY_Google_Analytics_Id, // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
   ],
 };
