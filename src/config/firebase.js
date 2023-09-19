e59f48877f15a3ca2b03f1f0d3f8d1c9ab6422cc
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-09-19 21:33:01 +0530

    fix: build errors

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: process.env.GATSBY_Api_Key,
-  authDomain: process.env.GATSBY_Auth_Domain,
-  projectId: process.env.GATSBY_Project_Id,
-  storageBucket: process.env.GATSBY_Storage_Bucket,
-  messagingSenderId: process.env.GATSBY_Messaging_Sender_Id,
-  appId: process.env.GATSBY_App_Id,
-  measurementId: process.env.GATSBY_Measurement_Id,
+export const firebaseConfig = {
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APP_ID,
+  measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
