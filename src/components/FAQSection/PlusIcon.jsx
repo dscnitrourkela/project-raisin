4a329ba0de0fbe6a89ef69f7a728b9f5b95dd248
Jayesh Nayak <starkjay2107@gmail.com>
2023-09-25 21:25:39 +0530

    feat: added faq section (#64)
    * feat: added faq section
    
    * fix: prettier check
    
    * fix: requested changes in faq section

---
+++ src/components/FAQSection/PlusIcon.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const PlusContainer = styled.div`
+  ${tw`
+    w-4
+    h-4
+    p-0
+    m-0
+    relative
+  `}
+`;
+const BarOne = styled.div`
+  ${tw`
+  `}
+  background-color: ${(props) => (props.active ? '#91F9D3' : '#D9D9D9')};
+  width: 16px;
+  height: 2px;
+  position: absolute;
+  top: 7px;
+  left: 0;
+  transition: transform 100ms linear;
+  transform: ${(props) => (props.active ? 'rotate(180deg)' : 'rotate(90deg)')};
+`;
+
+const BarTwo = styled.div`
+  ${tw`
+    
+  `}
+  background-color: ${(props) => (props.active ? '#91F9D3' : '#D9D9D9')};
+  width: 16px;
+  height: 2px;
+  transform: rotate(0deg);
+  position: absolute;
+  top: 7px;
+  left: 0;
+`;
+
+const PlusIcon = ({ active }) => (
+  <PlusContainer>
+    <BarOne active={active} />
+    <BarTwo active={active} />
+  </PlusContainer>
+);
+
+export default PlusIcon;
