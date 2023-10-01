0c615cd748d6816b3cb3b19c616ac90a064ffbf9
Lucif3r-in <ashutosh123rath@gmail.com>
2023-10-01 08:53:34 +0530

    fix: AboutUs content

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,13 +1,18 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { About } from '../components/shared';
+import AboutUs from '../../config/content/AboutUs';
 
 const Playground = () => (
-  <Helmet>
-    <meta charset='utf-8' />
-    <title>Playground</title>
-    <meta name='description' content='This is playground' />
-  </Helmet>
+  <>
+    <Helmet>
+      <meta charset='utf-8' />
+      <title>Playground</title>
+      <meta name='description' content='This is playground' />
+    </Helmet>
+    <About title={AboutUs.title} desc={AboutUs.desc} link={AboutUs.link} />
+  </>
 );
 
 export default Playground;
