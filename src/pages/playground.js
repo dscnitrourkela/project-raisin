0027d5abe702ae44cb0007483808f0a7a73ded31
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-10-03 03:25:01 +0530

    fix: add dummy event

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -3,8 +3,37 @@ import { Helmet } from 'react-helmet';
 import CardComponent from '../components/EventCard/Card';
 import useEvents from '../utils/useEvents';
 
+const events = [{
+  title: 'Game Of Thrones',
+  club: 'Knights Club',
+  venue: 'LA',
+  time: '10:00 AM',
+  date: '17',
+  month: 'July',
+  prizes: 'Cash prizes and trophies',
+  description: [{
+    id: 0,
+    desc: `Game of Thrones is a strategy board game created by Christian T. Petersen
+     and released by Fantasy Flight Games in 2003. The game is based on the A Song 
+     of Ice and Fire fantasy series by George R. R. Martin. It was followed in 2004
+      by the expansion A Clash of Kings, and in 2006 by the expansion A Storm of 
+      Swords. A Game of Thrones allows the players to take on the roles of several
+       of the Great Houses vying for control of the Seven Kingdoms, including House
+        Stark, House Lannister, House Baratheon, House Greyjoy, House Tyrell, and a
+         of the expansion A Clash of Kings, House Martell. Players maneuver armies to
+          secure support in the various regions that comprise the Seven Kingdoms, with 
+          the goal of capturing enough support to claim the Iron Throne. The game 
+          is designed for three to six players, who each begin with control of one of the six Great
+           Houses of Westeros. Randomly chosen neutral territories and land areas make up the 
+           remainder of the board.`,
+  }],
+  contact: ['Rashmi - 9848948949'],
+  poster: 'https://res.cloudinary.com/dw9odubkt/image/upload/v1695829352/Rectangle_1_wqmr3i.png',
+}];
+
 const Playground = () => {
-  const { events } = useEvents('technical');
+  // eslint-disable-next-line no-unused-vars
+  const { events: backendEvents } = useEvents('technical');
 
   return (
     <>
