8e453576a7902ee426cce2a4da86c8951527ac0e
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-10-11 07:59:02 +0000

    fix: test content (#88)

--- config/content/payment.js
+++ config/content/payment.js
@@ -4,7 +4,7 @@ export const PaymentDetails = ({ email, name, mobile }) => {
   return {
     purpose: 'INNOVISION 2023 | Registration Fee',
     date: date.toDateString(),
-    amount: 1000,
+    amount: 10,
     email,
     buyerName: name,
     phone: mobile,
