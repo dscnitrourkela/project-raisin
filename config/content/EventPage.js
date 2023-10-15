fba5ccb7a43c7195e7d6d9bcf408e30feba8daa1
Jayesh Nayak <starkjay2107@gmail.com>
2023-10-15 17:26:39 +0530

    fix: updated compressed images

--- config/content/EventPage.js
+++ config/content/EventPage.js
@@ -2,46 +2,50 @@ export default [
   {
     title: 'Technical',
     type: 'Technical',
-    image: 'https://res.cloudinary.com/dme9vltjf/image/upload/v1697275459/Tech_Event_wr6lho.jpg',
+    image:
+      'https://res.cloudinary.com/dme9vltjf/image/upload/' +
+      'v1697370432/Inno%202k23/david-leveque-GpNOhig3LSU-unsplash_11zon_y5ajhn.jpg',
   },
   {
     title: 'Pro Shows',
     type: 'Pro',
-    image: 'https://res.cloudinary.com/dme9vltjf/image/upload/v1697275441/Pro_Sho_t0vsrz.png',
+    image:
+      'https://res.cloudinary.com/dme9vltjf/image/upload/' +
+      'v1697370431/Inno%202k23/Pro_Sho_11zon_bowwb3.jpg',
   },
   {
     title: 'Workshops',
     type: 'Workshop',
     image:
-      'https://res.cloudinary.com/dme9vltjf/image/upload' +
-      '/v1697275448/lexscope-0k2mMcdTOYU-unsplash_qhr9gq.jpg',
+      'https://res.cloudinary.com/dme9vltjf/image/upload/v1697370432/' +
+      'Inno%202k23/lexscope-0k2mMcdTOYU-unsplash_11zon_zemsvp.jpg',
   },
   {
     title: 'Guest Lectures',
     type: 'Guest-Lectures',
     image:
-      'https://res.cloudinary.com/dme9vltjf/image/upload/' +
-      'v1697275443/miguel-henriques-RfiBK6Y_upQ-unsplash_hrzqmf.jpg',
+      'https://res.cloudinary.com/dme9vltjf/image/upload/v1697370431/' +
+      'Inno%202k23/miguel-henriques-RfiBK6Y_upQ-unsplash_11zon_a1nvuc.jpg',
   },
   {
     title: 'Exhibitions',
     type: 'Exhibitions',
     image:
-      'https://res.cloudinary.com/dme9vltjf/image/' +
-      'upload/v1697275444/sergio-rota-lq-3u9EqNb4-unsplash_u8p2q8.jpg',
+      'https://res.cloudinary.com/dme9vltjf/image/upload/' +
+      'v1697370431/Inno%202k23/sergio-rota-lq-3u9EqNb4-unsplash_11zon_mpy2wj.jpg',
   },
   {
     title: 'Fun Events',
     type: 'Fun',
     image:
-      'https://res.cloudinary.com/dme9vltjf/image/upload/' +
-      'v1697275446/jonny-gios-8wKTIZ1KEzY-unsplash_asltvy.jpg',
+      'https://res.cloudinary.com/dme9vltjf/image/upload/v1697370432/' +
+      'Inno%202k23/jonny-gios-8wKTIZ1KEzY-unsplash_11zon_frv6up.jpg',
   },
   {
     title: 'Others',
     type: 'Other',
     image:
-      'https://res.cloudinary.com/dme9vltjf/image/upload/' +
-      'v1697276377/samantha-gades-fIHozNWfcvs-unsplash_bi17wn.jpg',
+      'https://res.cloudinary.com/dme9vltjf/image/upload/v1697370957/' +
+      'Inno%202k23/samantha-gades-fIHozNWfcvs-unsplash_bi17wn_3_evkqzx.jpg',
   },
 ];
