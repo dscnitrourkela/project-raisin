3073a5e8007cc98de064ecc659e7ba1c234f71ad
Jayesh Nayak <starkjay2107@gmail.com>
2023-10-20 16:57:03 +0530

    feat: added CA Page

---
+++ config/content/CA.js
@@ -0,0 +1,34 @@
+export const CADetails = {
+  title: 'Campus Ambassador Program',
+  content: [
+    {
+      title: 'Perks',
+      list: [
+        'Certificate of appreciation',
+        'Free merchandise and gift hampers on completion of certain milestones',
+        'Free of cost access to webinar and workshop',
+        'Publicity in social media handles',
+        'Special discount for next fest by NIT Rourkela',
+        'Building network and personal connections',
+      ],
+    },
+    {
+      title: 'Procedure',
+      list: [
+        'Register yourself for INNOVISION at inno.nitrkl.in',
+        'Go to CA program',
+        'Click on Register as CA',
+        'Register as CA',
+        'Your Mobile Number is your Unique Referral Code',
+        'Help your friends register themselves and apply the unique Referral code',
+        'Win Goodies and Hampers',
+      ],
+    },
+  ],
+  button: {
+    text: 'Register as Campus Ambassador',
+    link: 'https://google.com',
+  },
+  buttonText: 'Register as Campus Ambassador',
+  buttonLink: 'https://google.com',
+};
---
+++ src/components/CAProgram/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const PerksContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    gap-2
+  `}
+`;
+
+export const ItemsContainer = styled.div`
+  ${tw`
+    w-[95%]
+    mx-auto
+    flex
+    flex-col
+    gap-10
+    mt-10
+  `}
+
+  @media (max-width: 512px) {
+    width: 100%;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  ${tw`
+    md:w-[20%]
+    mx-auto
+    w-[40%]
+    flex
+    justify-center
+  `}
+`;
