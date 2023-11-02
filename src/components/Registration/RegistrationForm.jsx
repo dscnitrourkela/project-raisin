ee3039fd045e80abc711fa0d2cbb984b8636d75d
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-11-02 18:33:24 +0530

    chore: revert closing of registration

--- src/components/Registration/RegistrationForm.jsx
+++ src/components/Registration/RegistrationForm.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { navigate } from 'gatsby';
 import { toast } from 'react-toastify';
 import { RegistrationContext } from './RegistrationContext';
-import { Body1, Body2, ButtonText, Heading2, Heading4, Input } from '../shared';
+import { Body2, ButtonText, Heading2, Input } from '../shared';
 import { InputContainer, RegistrationCard, RegistrationCardTitle } from './styles';
 import Button from '../shared/Button';
 import { SHOW, verify, verifyAll } from './utils';
@@ -60,61 +60,57 @@ export const RegistrationForm = () => {
     <RegistrationCard>
       <RegistrationCardTitle>
         <Heading2 style={{ textTransform: 'none' }}>
-          {isNITR ? 'Registration form' : 'Registration has been closed'}
+          {/* {isNITR ? 'Registration form' : 'Registration has been closed'} */}
+          Registration form
         </Heading2>
-        {isNITR && (
-          <Body2 style={{ display: 'flex', gap: '16px' }}>
-            Student from NIT Rourkela
-            <ButtonText
-              onClick={() => setIsNITR((prev) => !prev)}
-              className='cursor-pointer'
-              outline
-            >
-              Change
-            </ButtonText>
-          </Body2>
-        )}
+        <Body2 style={{ display: 'flex', gap: '16px' }}>
+          {isNITR ? 'Student from NIT Rourkela' : 'Student from other college'}
+          <ButtonText onClick={() => setIsNITR((prev) => !prev)} className='cursor-pointer' outline>
+            Change
+          </ButtonText>
+        </Body2>
       </RegistrationCardTitle>
+      {/* {isNITR ? ( */}
+      <>
+        <InputContainer>
+          {Object.entries(inputData)
+            .filter(([, value]) =>
+              isNITR ? SHOW.NITR.includes(value.show) : SHOW.NON_NITR.includes(value.show),
+            )
+            .map(([key, value], index, array) => {
+              let gridCols;
+              if (array.length <= 4) {
+                gridCols = 'span 4';
+              } else if (index === array.length - 1 && index % 2 === 0) {
+                gridCols = '2 / 4';
+              } else {
+                gridCols = 'span 2';
+              }
 
-      {isNITR ? (
-        <>
-          <InputContainer>
-            {Object.entries(inputData)
-              .filter(([, value]) => SHOW.NITR.includes(value.show))
-              .map(([key, value], index, array) => {
-                let gridCols;
-                if (array.length <= 4) {
-                  gridCols = 'span 4';
-                } else if (index === array.length - 1 && index % 2 === 0) {
-                  gridCols = '2 / 4';
-                } else {
-                  gridCols = 'span 2';
-                }
+              return (
+                <div key={key} style={{ gridColumn: gridCols }}>
+                  <Input
+                    data={value}
+                    key={key}
+                    onChange={(e) => setInputValue(key, e.target.value)}
+                    onBlur={onBlur}
+                  />
+                </div>
+              );
+            })}
+        </InputContainer>
 
-                return (
-                  <div key={key} style={{ gridColumn: gridCols }}>
-                    <Input
-                      data={value}
-                      key={key}
-                      onChange={(e) => setInputValue(key, e.target.value)}
-                      onBlur={onBlur}
-                    />
-                  </div>
-                );
-              })}
-          </InputContainer>
-
-          <Button
-            type='submit'
-            variant='filled'
-            text='Register'
-            width='350px'
-            disabled={!verified && isNITR}
-            tooltip={verified ? '' : 'Verify your credentials'}
-            onClick={registerUser}
-          />
-        </>
-      ) : (
+        <Button
+          type='submit'
+          variant='filled'
+          text='Register'
+          width='350px'
+          disabled={!verified && isNITR}
+          tooltip={verified ? '' : 'Verify your credentials'}
+          onClick={registerUser}
+        />
+      </>
+      {/* ) : (
         <>
           <Heading4 font='Inter' style={{ textAlign: 'center', maxWidth: '500px' }}>
             Registration for non-NITR students has been closed.
@@ -129,7 +125,7 @@ export const RegistrationForm = () => {
             </ButtonText>
           </Body1>
         </>
-      )}
+      )} */}
     </RegistrationCard>
   );
 };
