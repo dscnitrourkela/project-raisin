a560d34387bd4cf4123eb9fc0d667897af3ddf99
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2024-09-19 18:45:40 +0000

    chore: clean earlier code

--- config/content/index.js
+++
@@ -1,9 +0,0 @@
-export { default as nav } from './Navigation';
-export { default as footer } from './Footer';
-export * from './FAQ';
-export { default as success } from './Success';
-export { default as pageNotFound } from './PageNotFound';
-export { default as _sponsor } from './sponsors';
-export { default as _event } from './Events';
-export { default as _eventPage } from './EventPage';
-export * from './payment';
