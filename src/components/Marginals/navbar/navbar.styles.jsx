5dd7c1f7053af943c60fc677c7e953916f5ad9ad
Harsh <harshtanishq2002@gmail.com>
2024-10-01 19:49:14 +0530

    fix: made required changes in CSS, fix: fixed navbar padding, fixed order of logo

--- src/components/Marginals/navbar/navbar.styles.jsx
+++ src/components/Marginals/navbar/navbar.styles.jsx
@@ -1,24 +1,22 @@
+import { PrimaryButton, SecondaryButton } from '@/components/shared/Typography/Buttons';
+import { NavbarLink } from '@/components/shared/Typography/Links';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
 export const NavContainer = styled.nav`
-  ${tw`fixed top-0 w-full h-10 max-w-7xl p-3 mx-auto mt-4 sm:h-12 md:h-14 lg:h-16 sm:p-4 md:p-5 sm:mt-6 md:mt-7 left-[50%] z-[1000] translate-x-[-50%]`}
+  ${tw`fixed top-0 w-full h-10 max-w-[1400px] p-3 mx-auto mt-4 sm:h-12 md:h-14 lg:h-16 sm:p-4 md:p-5 sm:mt-6 md:mt-7 left-[50%] z-[1000] translate-x-[-50%]`}
 `;
 
 export const NavCover = styled.div`
-  ${tw`flex justify-between gap-[150px]`}
+  ${tw`flex justify-between lg:gap-[190px] md:gap-[120px] sm:gap-[70px]`}
 `;
 
 export const Logos = styled.div`
   ${tw`flex mt-3 space-x-4 justify-evenly`}
 `;
 
-export const LogoItem = styled.div`
-  backdrop-filter: blur(11.075665473937988px);
-
-  ${tw`
-    w-10 h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 text-white text-[14px] rounded-full font-montserrat font-medium cursor-pointer border-[0.9px] border-[#ffffff66] bg-[#ffffff0d] flex justify-center items-center transition-all duration-300 hover:scale-110 hover:shadow-md
-  `}
+export const LogoItem = styled(SecondaryButton)`
+  ${tw`flex items-center justify-center w-10 h-10 p-0 md:w-8 md:h-8 lg:w-10 lg:h-10 hover:scale-110 hover:shadow-md`}
 
   &:hover {
     box-shadow: 0 0 8px #ffffff66 0 0 12px #ffffff4d;
@@ -35,25 +33,16 @@ export const MainBar = styled.nav`
   backdrop-filter: blur(13.681711196899414px);
 
   ${tw`
-    inline-flex items-center gap-5 rounded-md border-[1.14px] border-solid border-white/[0.06] bg-white/[0.02] flex justify-evenly w-full max-[930px]:hidden max-w-xs md:max-w-md lg:max-w-xl space-x-0.5 rounded-md border-[0.5px] py-1 lg:mr-0 md:mr-[50px] /* Reduced max width and padding */
+    inline-flex items-center rounded-md border-[1.14px] border-solid border-white/[0.06] bg-white/[0.02] p-2 pr-6 flex justify-between  w-full max-[930px]:hidden max-w-xs md:max-w-md lg:max-w-xl space-x-0.5 rounded-md border-[0.5px] py-1 lg:mr-0 md:mr-[50px]
   `}
 `;
 
-export const MainBarItems = styled.div`
-  opacity: ${({ $isActive }) => ($isActive ? '1' : '0.3')};
-
-  ${tw` text-xs md:text-sm not-italic font-medium transition-all duration-300 cursor-pointer text-white font-montserrat leading-[140%] hover:opacity-100 pt-1`}
+export const MainBarItems = styled(NavbarLink)`
+  ${tw`text-xs md:text-sm`}
 `;
 
-export const RegisterButton = styled.button`
-  background: radial-gradient(219.1% 188.15% at 52.58% -68.5%, #ff8dff 30.52%, #ff3c7f 53.85%);
-  box-shadow:
-    0px 10px 149.474px 0px #ffe3697a,
-    0px 0px 0.83px 3.322px #ffffff1a,
-    0px -3.322px 1.661px 0px #00000040 inset,
-    0px 1.661px 0.83px 0px #ffffff40 inset;
-
-  ${tw` max-[930px]:hidden w-[150px]  h-[45px]  rounded-md  hover:border-[2px]  hover:border-slate-500  transition-all  duration-300 mt-2 lg:ml-0 md:ml-[-7rem]`}
+export const RegisterButton = styled(PrimaryButton)`
+  ${tw` max-[930px]:hidden lg:ml-0 md:ml-[-7rem]`}
 `;
 
 export const HamburgerContainer = styled.div`
--- src/config/content/NavbarData/NavData.js
+++ src/config/content/NavbarData/NavData.js
@@ -4,12 +4,12 @@ export const logos = [
     link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1726977580/Frame_2087326314_uhhz9b.png',
   },
   {
-    name: 'Insta',
-    link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1726908562/insta_logo_v7l8o8.png',
-  },
-  {
     name: 'X',
     link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1726908562/X_logo_seqxxb.png',
+  },
+  {
+    name: 'Insta',
+    link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1726908562/insta_logo_v7l8o8.png',
   },
   {
     name: 'Linkden',
