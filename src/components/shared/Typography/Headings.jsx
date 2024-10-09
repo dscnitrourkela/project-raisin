88acfad4e82028fa36f3e3d6c3210d069fe44c83
AYANscyy2 <mayan6378@gmail.com>
2024-10-09 16:11:26 +0530

    feat: improved file structure and fix prettier issues

--- src/components/shared/Typography/Headings.jsx
+++ src/components/shared/Typography/Headings.jsx
@@ -11,12 +11,9 @@ export const Heading2 = styled.h2`
 
 export const LogoText = styled.h1`
   ${tw`text-[80px] xsm:text-[120px]  md:text-[150px] lg:text-[170px] not-italic font-normal font-techno text-[#FFFFFFE6]`}
-
   text-shadow: 0px 0px 18.006px #FFFFFF66;
 `;
 export const SubLogoText = styled.h1`
-  ${tw`text-[60px] xsm:text-[70px] md:text-[80px] lg:text-[100px] not-italic font-normal font-techno text-transparent`}
-
-  -webkit-text-stroke: 2px #ffffffe6;
+  ${tw`text-[60px] xsm:text-[70px] md:text-[80px] lg:text-[100px] not-italic font-normal font-techno text-transparent`} -webkit-text-stroke: 2px #ffffffe6;
   text-shadow: none;
 `;
