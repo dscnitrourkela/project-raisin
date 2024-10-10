ac118b53cfc589c9125916598307e31f4713efc8
ayush <ayushatschool5@gmail.com>
2024-10-10 13:39:48 +0530

    fix: resolve conflicts

--- src/components/HOC/Hoc.jsx
+++ src/components/HOC/Hoc.jsx
@@ -4,8 +4,9 @@ import Navbar from '../Marginals/navbar/navbar';
 function Hoc({ children }) {
   return (
     <>
-      <Navbar />
-      <div className='pt-20'>{children}</div>
+      {/* <Navbar /> */}
+      {/* <div className='pt-20'>{children}</div> */}
+      {children}
       <Footer />
     </>
   );
