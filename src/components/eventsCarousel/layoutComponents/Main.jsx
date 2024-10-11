34f361f23d1aad538f4b5b0b5e3330c3d82062e2
Himanshu Kaushik <hk9797592893@gmail.com>
2024-10-11 10:51:27 +0530

    chore: added margin to event heading

--- src/components/eventsCarousel/layoutComponents/Main.jsx
+++ src/components/eventsCarousel/layoutComponents/Main.jsx
@@ -5,7 +5,7 @@ export const EventCarousalMain = () => {
   return (
     <>
       <div className='-mt-32 min-h-screen w-full flex flex-col mb-44'>
-        <div className='ml-10'>
+        <div className='mt-7'>
           <Heading1>EVENTS</Heading1>
         </div>
         <SliderEventsWrapper />
