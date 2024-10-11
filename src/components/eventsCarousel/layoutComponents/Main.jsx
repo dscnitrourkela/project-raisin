3cee2a20aa19ebf4e8a6d193be549ad24d1ddc18
Himanshu Kaushik <hk9797592893@gmail.com>
2024-10-11 16:48:58 +0530

    chore: fixed event heading (#144)
    * chore: improved about section and footer
    
    * chore: improved about us
    
    * chore: added margins to event heading
    
    * chore: fixed event heading

--- src/components/eventsCarousel/layoutComponents/Main.jsx
+++ src/components/eventsCarousel/layoutComponents/Main.jsx
@@ -4,8 +4,8 @@ import { SliderEventsWrapper } from '../wrapperComponents/EventWrapper';
 export const EventCarousalMain = () => {
   return (
     <>
-      <div className='-mt-32 min-h-screen w-full flex flex-col mb-44'>
-        <div className='ml-10'>
+      <div className='-mt-60 min-h-screen w-full flex flex-col mb-44'>
+        <div className='mt-32 lg:mt-5'>
           <Heading1>EVENTS</Heading1>
         </div>
         <SliderEventsWrapper />
