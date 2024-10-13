3c5f4307bb23717122dd65e47a048106f1df9640
AYANscyy2 <mayan6378@gmail.com>
2024-10-13 20:39:58 +0530

    feat: fix minor issue

--- src/components/EventsSection/Header.jsx/SubHeader.jsx
+++ src/components/EventsSection/Header.jsx/SubHeader.jsx
@@ -2,7 +2,7 @@ export const SubHeader = () => {
   return (
     <>
       <div className='h-[80px] w-full bg-[#424242] flex flex-col md:flex-row justify-center items-center  md:gap-36'>
-        <div className='font-orbitron font-normal text-[17px] xsm:text-[24px] leading-[36,67] text-white'>
+        <div className='font-spaceX font-normal text-[17px] xsm:text-[24px] leading-[36,67] text-white'>
           TOTAL EVENTS 43
         </div>
         <div className='font-spaceX font-normal text-[17px] xsm:text-[24px] leading-[36,67] text-white'>
