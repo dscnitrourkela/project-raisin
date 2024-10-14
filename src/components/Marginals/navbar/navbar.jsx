a40df20b3b3aa743d3dee702f312f205566be2a6
Harsh <140698710+Cybrite@users.noreply.github.com>
2024-10-14 18:19:40 +0530

    feat: added new navbar (#159)
    * feat: added new navbar
    
    * feat: added new BG, new links in hero
    
    * fix: fixed stats background
    
    * fix: content change

--- src/components/Marginals/navbar/navbar.jsx
+++ src/components/Marginals/navbar/navbar.jsx
@@ -2,23 +2,19 @@
 import React from 'react';
 import {
   HamburgerContainer,
-  LogoItem,
-  Logos,
   MainBar,
   MainBarItems,
   MenuLogoItem,
   NavContainer,
   NavCover,
-  RegisterButton,
   ResItem,
   ResList,
   ResMen,
-  ResponsiveMenu,
   HamburgerRegisterButton,
 } from './navbar.styles';
 import Hamburger from 'hamburger-react';
 import { useState } from 'react';
-import { ButtonData, logos, MainNavData } from '../../../config/content/NavbarData/NavData';
+import { ButtonData, logos, navLinks } from '../../../config/content/NavbarData/NavData';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -32,42 +28,32 @@ const Navbar = () => {
   return (
     <NavContainer>
       <NavCover>
-        <Logos>
-          {logos.slice(1).map((logo, index) => (
-            <LogoItem key={index}>
-              <Image src={logo.link} alt={logo.name} width='20' height='20' />
-            </LogoItem>
-          ))}
-        </Logos>
         <MainBar>
           <MenuLogoItem>
-            <Link href='/playground'>
+            <Link href='/'>
               <Image src={logos[0].link} alt={logos[0].name} width='40' height='40' />
             </Link>
           </MenuLogoItem>
-          {MainNavData.map((item, index) => (
-            <MainBarItems key={index}>{item.title}</MainBarItems>
+          {navLinks.map((navLink) => (
+            <MainBarItems key={navLink.id}>{navLink.name}</MainBarItems>
           ))}
         </MainBar>
-        <RegisterButton>
-          <Link href={ButtonData.link}>{ButtonData.title}</Link>
-        </RegisterButton>
-
         <HamburgerContainer>
-          <Hamburger toggled={isOpen} toggle={handleToggle}></Hamburger>
+          <Hamburger toggled={isOpen} toggle={handleToggle} />
         </HamburgerContainer>
       </NavCover>
 
       {isOpen ? (
         <ResMen>
           <ResList>
-            {MainNavData.map((item, index) => (
-              <ResItem key={index}>{item.title}</ResItem>
+            {navLinks.map((navLink) => (
+              <ResItem key={navLink.id}>{navLink.name}</ResItem>
             ))}
+
+            <HamburgerRegisterButton>
+              <Link href={ButtonData.link}>{ButtonData.title}</Link>
+            </HamburgerRegisterButton>
           </ResList>
-          <HamburgerRegisterButton>
-            <Link href={ButtonData.link}>{ButtonData.title}</Link>
-          </HamburgerRegisterButton>
         </ResMen>
       ) : null}
     </NavContainer>
--- src/config/content/NavbarData/NavData.js
+++ src/config/content/NavbarData/NavData.js
@@ -1,39 +1,15 @@
 export const logos = [
   {
     name: 'inno',
-    link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1726977580/Frame_2087326314_uhhz9b.png',
-  },
-  {
-    name: 'WhatsApp',
-    link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1728548889/WhatsApp_ly1u9i.png',
-  },
-  {
-    name: 'Insta',
-    link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1726908562/insta_logo_v7l8o8.png',
-  },
-  {
-    name: 'Gmail',
-    link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1728548889/Gmail_orgt0y.png',
+    link: 'https://res.cloudinary.com/dqqyuvg1v/image/upload/v1728826246/Frame_2087326348_opiovp.png',
   },
 ];
 
-export const MainNavData = [
-  {
-    title: 'About us',
-    link: 'about',
-  },
-  {
-    title: 'Events',
-    link: 'events',
-  },
-  {
-    title: 'Sponsors',
-    link: 'sponsors',
-  },
-  {
-    title: 'Contact us',
-    link: 'contact',
-  },
+export const navLinks = [
+  { name: 'About us', id: 'about us' },
+  { name: 'Events', id: 'events' },
+  { name: 'Sponsers', id: 'sponsers' },
+  { name: 'Contact Us', id: 'contact' },
 ];
 
 export const ButtonData = {
