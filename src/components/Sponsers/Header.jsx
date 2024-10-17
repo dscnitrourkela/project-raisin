2778bebad5aaf16bffaf55e4ad6d8394cf679f71
Harsh <harshtanishq2002@gmail.com>
2024-10-17 22:44:21 +0530

    fix: color changes

--- src/components/Sponsers/Header.jsx
+++ src/components/Sponsers/Header.jsx
@@ -2,8 +2,8 @@ import { BodyTitle } from '../AboutUs/body.styles';
 
 export const Header = () => {
   return (
-    <>
+    <div className='bg-black bg-opacity-50'>
       <BodyTitle style={{ marginBottom: '0px' }}> Sponsors</BodyTitle>
-    </>
+    </div>
   );
 };
