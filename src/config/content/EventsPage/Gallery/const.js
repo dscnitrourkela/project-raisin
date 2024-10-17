ece480c5b126ef75de83caff48e84184a1132cd7
Siddharth <siddharthnmishra7@gmail.com>
2024-10-17 20:31:39 +0530

    feat: add gallery section in events page

---
+++ src/config/content/EventsPage/Gallery/const.js
@@ -0,0 +1,6 @@
+export const galleryImages = [
+  { src: 'https://picsum.photos/id/1011/800/400', alt: 'Beautiful Landscape 1' },
+  { src: 'https://picsum.photos/id/1025/800/400', alt: 'Beautiful Landscape 2' },
+  { src: 'https://picsum.photos/id/1035/800/400', alt: 'Beautiful Landscape 3' },
+  { src: 'https://picsum.photos/id/1040/800/400', alt: 'Beautiful Landscape 4' },
+];
