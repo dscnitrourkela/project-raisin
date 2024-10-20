d9057eb5ce83de426fe8e0dc1ad59c105e014e12
Dhrutinandan Swain <92504849+swaindhruti@users.noreply.github.com>
2024-10-20 20:01:44 +0530

    feat(navbar): auto-close hamburger menu on navigation (#188)
    * refactor: minor ui update
    
    * feat: add modals for terms&conditions and code of conduct
    
    * refactor: add responivesness to events card in events page
    
    * feat: auto-close hamburger menu on navigation item click

--- src/components/Marginals/navbar/navbar.jsx
+++ src/components/Marginals/navbar/navbar.jsx
@@ -17,13 +17,23 @@ import Hamburger from 'hamburger-react';
 import { ButtonData, logos, navLinks } from '../../../config/content/NavbarData/NavData';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
   function handleToggle() {
     setIsOpen(!isOpen);
   }
+
+  const handleCloseMenu = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    handleCloseMenu();
+  }, [pathname]);
 
   return (
     <NavContainer>
@@ -31,18 +41,22 @@ const Navbar = () => {
         <NavCover>
           <MainBar>
             <MenuLogoItem>
-              <Link href='/'>
+              <Link href='/' onClick={handleCloseMenu}>
                 <Image src={logos[0].link} alt={logos[0].name} width='40' height='40' />
               </Link>
             </MenuLogoItem>
             {navLinks.map((navLink) => (
               <MainBarItems key={navLink.link}>
-                <Link href={`${navLink.link}`}>{navLink.name}</Link>
+                <Link href={`${navLink.link}`} onClick={handleCloseMenu}>
+                  {navLink.name}
+                </Link>
               </MainBarItems>
             ))}
           </MainBar>
           <RegisterButton>
-            <Link href='/register'>{ButtonData.title}</Link>
+            <Link href='/register' onClick={handleCloseMenu}>
+              {ButtonData.title}
+            </Link>
           </RegisterButton>
           <HamburgerContainer>
             <Hamburger toggled={isOpen} toggle={handleToggle} size={20} />
@@ -56,12 +70,16 @@ const Navbar = () => {
             <ResList>
               {navLinks.map((navLink) => (
                 <ResItem key={navLink.link}>
-                  <Link href={`${navLink.link}`}>{navLink.name}</Link>
+                  <Link href={`${navLink.link}`} onClick={handleCloseMenu}>
+                    {navLink.name}
+                  </Link>
                 </ResItem>
               ))}
 
               <HamburgerRegisterButton>
-                <Link href={ButtonData.link}>{ButtonData.title}</Link>
+                <Link href={ButtonData.link} onClick={handleCloseMenu}>
+                  {ButtonData.title}
+                </Link>
               </HamburgerRegisterButton>
             </ResList>
           </ResMen>
