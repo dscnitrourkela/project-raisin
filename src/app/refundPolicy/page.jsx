07c624d413fd42bc684e2ae00a419cb4d15023dc
Dhrutinandan Swain <dhrutiswainnandan@gmail.com>
2024-10-21 21:08:37 +0530

    chore: minor file refactor

--- src/app/refundPolicy/page.jsx
+++ src/app/refundPolicy/page.jsx
@@ -1,16 +1,16 @@
 'use client';
-import { EventsContainer, Heading3, Heading4 } from './page.styles';
+import { PaymentContainer, Heading3, Heading4 } from './page.styles';
 import { PaymentPolicyPageData } from '@/config/content/Footer';
 
 export default function Page() {
   return (
-    <EventsContainer>
+    <PaymentContainer>
       {PaymentPolicyPageData.map((item) => (
         <div key={item.id}>
           <Heading3>{item.title}</Heading3>
           <Heading4>{item.description}</Heading4>
         </div>
       ))}
-    </EventsContainer>
+    </PaymentContainer>
   );
 }
--- src/app/refundPolicy/page.styles.jsx
+++ src/app/refundPolicy/page.styles.jsx
@@ -2,7 +2,7 @@ import tw from 'twin.macro';
 import styled from 'styled-components';
 import { Heading2 } from '@/components/shared/Typography/Headings';
 
-export const EventsContainer = styled.div`
+export const PaymentContainer = styled.div`
   background-image: url('https://res.cloudinary.com/dhv234qct/image/upload/v1728888341/Inno2k24/yupqoznoucyhxwchhbv7.png');
   ${tw`w-full flex flex-col bg-cover pt-24 pl-4 xxxs:pl-8 pr-8 xsm:pt-48 md:pt-52`}
 `;
