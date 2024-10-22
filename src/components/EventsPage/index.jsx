3377636a5dff7eaeae40443859130954f749ef25
ayush <ayushatschool5@gmail.com>
2024-10-22 22:44:33 +0530

    feat: added seo2

--- src/components/EventsPage/index.jsx
+++ src/components/EventsPage/index.jsx
@@ -1,20 +1,7 @@
-import EventStructuredData from '@/components/SEO/EventStructureData';
 import { SliderEventsWrapper } from './Carousel/PreviewCarousel/EventWrapper';
 const MainCarousel = ({ previewItems, descriptionItems }) => {
   return (
     <>
-      {descriptionItems.length !== 0 &&
-        descriptionItems.map((eventData) => (
-          <EventStructuredData
-            description={eventData.Description}
-            endDate={'2024-11-10T23:59:59+00:00'}
-            location={eventData.Location}
-            name={eventData.Heading}
-            organizerName={'INNOVISION'}
-            startDate={'2024-11-08T00:00:00+00:00'}
-            key={eventData.Heading}
-          />
-        ))}
       <SliderEventsWrapper previewItems={previewItems} descriptionItems={descriptionItems} />
     </>
   );
--- src/components/SEO/EventStructureData.jsx
+++ src/components/SEO/EventStructureData.jsx
@@ -25,7 +25,6 @@ const EventStructuredData = ({
     },
   };
 
-  console.log(structuredData);
   return (
     <Head>
       <script
