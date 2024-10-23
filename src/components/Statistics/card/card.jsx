eb67a976691e4c6fe6b934a466fd769318272f08
Harsh <harshtanishq2002@gmail.com>
2024-10-23 17:44:35 +0530

    fix: added padding in stats table

--- src/components/Statistics/card/card.jsx
+++ src/components/Statistics/card/card.jsx
@@ -5,13 +5,13 @@ import { Table } from 'flowbite-react';
 
 export const Card = () => {
   return (
-    <div className='p-4 flex justify-center items-center'>
+    <div className='flex items-center justify-center p-4'>
       <div
-        className='w-full max-w-7xl bg-black rounded-lg shadow-lg overflow-hidden'
+        className='w-full overflow-hidden bg-black rounded-lg shadow-lg max-w-7xl'
         style={{ boxShadow: '0px 4px 31px 0px rgba(255, 255, 255, 0.25)' }}
       >
         <div className='p-4 sm:p-6 md:p-8 lg:p-10'>
-          <h2 className='text-center font-orbitron font-semibold text-white text-lg sm:text-xl md:text-2xl lg:text-3xl mb-4 sm:mb-6 md:mb-8'>
+          <h2 className='mb-4 text-lg font-semibold text-center text-white font-orbitron sm:text-xl md:text-2xl lg:text-3xl sm:mb-6 md:mb-8'>
             Top Colleges Registered for Innovision
           </h2>
           <div className='overflow-x-auto'>
@@ -20,7 +20,7 @@ export const Card = () => {
                 {Topics.map((topic, index) => (
                   <Table.HeadCell
                     key={index}
-                    className='font-orbitron font-bold text-center text-xs sm:text-sm md:text-base lg:text-lg py-2 sm:py-3 border-b border-gray-700'
+                    className='py-2 text-xs font-bold text-center border-b border-gray-700 font-orbitron sm:text-sm md:text-base lg:text-lg sm:py-3'
                   >
                     {topic}
                   </Table.HeadCell>
@@ -28,11 +28,11 @@ export const Card = () => {
               </Table.Head>
               <Table.Body className='divide-y divide-gray-700'>
                 {Info.map((item, i) => (
-                  <Table.Row className='hover:bg-gray-800 transition-all' key={i}>
-                    <Table.Cell className='font-montserrat text-white text-center font-medium text-xs sm:text-sm md:text-base py-2 sm:py-3'>
+                  <Table.Row className='transition-all hover:bg-gray-800' key={i}>
+                    <Table.Cell className='py-2 text-xs font-medium text-center text-white font-montserrat sm:text-sm md:text-base sm:py-6 2xs:py-5'>
                       {item.Rank}
                     </Table.Cell>
-                    <Table.Cell className='font-montserrat text-white text-center font-medium text-xs sm:text-sm md:text-base py-2 sm:py-3'>
+                    <Table.Cell className='py-2 text-xs font-medium text-center text-white font-montserrat sm:text-sm md:text-base sm:py-3'>
                       {item.college}
                     </Table.Cell>
                   </Table.Row>
