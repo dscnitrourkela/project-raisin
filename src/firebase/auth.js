7363adbfa60a95d3ba895084e7927f5ab7371f2a
ayush <ayushatschool5@gmail.com>
2024-10-24 18:08:58 +0530

    chore: add auth token

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,24 +1,25 @@
-// import { signInWithPopup, GoogleAuthProvider, signOut } from 'firebase/auth';
-// import { auth } from './firebase';
+import { signInWithPopup, GoogleAuthProvider, signOut } from 'firebase/auth';
+import { auth } from './firebase';
 
-// export const signInWithGoogle = async () => {
-//   const provider = new GoogleAuthProvider();
-//   try {
-//     const result = await signInWithPopup(auth, provider);
-//     const user = result.user;
-//     return user;
-//   } catch (error) {
-//     console.error('Error signing in with Google:', error);
-//     return false;
-//   }
-// };
+export const signInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    console.log(user);
+    return user;
+  } catch (error) {
+    console.error('Error signing in with Google:', error);
+    return false;
+  }
+};
 
-// export const signOutUser = async () => {
-//   try {
-//     await signOut(auth);
-//     return {};
-//   } catch (error) {
-//     console.error('Error signing out:', error);
-//     return false;
-//   }
-// };
+export const signOutUser = async () => {
+  try {
+    await signOut(auth);
+    return {};
+  } catch (error) {
+    console.error('Error signing out:', error);
+    return false;
+  }
+};
