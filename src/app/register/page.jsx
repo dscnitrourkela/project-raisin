f88e43fb0b45643bab0a280167804a5498c13172
Siddharth <siddharthnmishra7@gmail.com>
2024-10-25 16:10:45 +0530

    fix: minor changes

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -201,7 +201,7 @@ function Page() {
           <PaymentPolicyInfo>
             <Link href='/refundPolicy'>Please review the Payment Policy before registering.</Link>
             <br />
-            (NOTE: Registration Fees + Convenience Fees + GST will be applied)
+            (NOTE: Registration Fees (₹899) + Convenience Fees + GST will be applied)
           </PaymentPolicyInfo>
           <CampusAmbassador
             handleChange={handleChange}
