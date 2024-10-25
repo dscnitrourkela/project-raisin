7babbb24c651f3ba355f3dbb7a2c844563372a96
Shauryaman Sawai <sawaisushil@gmail.com>
2024-10-25 19:34:06 +0530

    fun_event

--- src/config/content/EventsPage/FunEvents/Data.js
+++ src/config/content/EventsPage/FunEvents/Data.js
@@ -1,57 +1,113 @@
 export const FunEvents_previewItems = [
   {
     id: 1,
-    ImageURL:
-      'https://res.cloudinary.com/dhv234qct/image/upload/v1729520868/FunEvents/vfk7zxch1vxsdzdaibep.jpg', // TBA
-    PreviewDescription: '',
+    ImageURL: 'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797486/Quick_Hand_aeeiun.jpg',
+    PreviewDescription:
+      'The Quick Hand Game is a thrilling test of speed and reflexes! Players dive into rapid-fire challenges grab in the blink of an eye. Whether for fun or focus, it’s guaranteed to keep you on your toes! Time: 9-5 ,Day : All three days',
     Rulebook: '',
   },
   {
     id: 2,
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621431/INNO_events/Inno%20Club%20Posters/whxm03bgnue8zsarqtih.jpg',
-    PreviewDescription: '',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797484/Guess_The_Character_hnrfn5.jpg',
+    PreviewDescription:
+      'Can you crack the code? Test your detecƟve skills and try to idenƟfy the mystery character hidden inside the box. Are you ready to take on the challenge?',
     Rulebook: '',
   },
   {
     id: 3,
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621429/INNO_events/Inno%20Club%20Posters/l9m6v9lkfioqht4d6mof.jpg',
-    PreviewDescription: '',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797482/Gel_Blaster_bdckk8.png',
+    PreviewDescription:
+      'Get ready for an epic baƩle! Grab your blaster and compete in intense, acƟon-packed matches. Show off your skills and emerge victorious in this thrilling event. Time: 9-5 Day : All three days',
     Rulebook: '',
   },
   {
     id: 4,
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621431/INNO_events/Inno%20Club%20Posters/gza47dud18pnjymfupjh.jpg',
-    PreviewDescription: '',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797483/Box_Cricket_t2tvi5.jpg',
+    PreviewDescription:
+      'Are you ready to showcase your crickeƟng skills? Join us for an exciƟng box cricket tournament. Assemble your team and compete for the championship. Let the games begin!',
     Rulebook: '',
   },
   {
     id: 5,
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621429/INNO_events/Inno%20Club%20Posters/dtcolsieyzwjkjpnmin4.jpg',
-    PreviewDescription: '',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797486/Soft_Archery_zmfbve.jpg',
+    PreviewDescription:
+      'Unleash your inner archer at Innovision 2024! Test your aim and have fun with soŌ-Ɵpped arrows. Whether you are a seasoned pro or a beginner, this exciƟng event offers a unique experience for all.',
     Rulebook: '',
   },
   {
     id: 6,
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621430/INNO_events/Inno%20Club%20Posters/l3re28rbhpqgj3z1qiqr.jpg',
-    PreviewDescription: '',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797484/Balloon_Shooting_qlpxxp.jpg',
+    PreviewDescription:
+      'Test your aim and accuracy in our fun-filled balloon shooƟng challenge. See how many balloons you can pop in a given Ɵme. Compete against friends and family and aim for the top spot!',
     Rulebook: '',
   },
   {
     id: 7,
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621430/INNO_events/Inno%20Club%20Posters/hzbnmxl0ypfqgtxtr7va.jpg',
-    PreviewDescription: '',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797484/Cup_Tic_Tac_Toe_vfa3l1.jpg',
+    PreviewDescription:
+      'Calling all Tic-Tac-Toe enthusiasts! Join us for a thrilling tournament at the Innovision . Show off your strategic skills and compete against fellow players in this fun-filled event. Whether you are a seasonedpro or just starƟng out, everyone is welcome to participate.',
     Rulebook: '',
   },
   {
     id: 8,
     ImageURL:
-      'https://res.cloudinary.com/dhv234qct/image/upload/v1729520803/FunEvents/lozbhvbxshwllkcm9fyk.jpg', // TBA
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797485/Body_Zorbing_podoll.jpg',
+    PreviewDescription:
+      'Get ready for a thrilling adventure! Experience the joy of body zorbing, a fun and exciƟng acƟvity where you roll around in a giant inflatable ball. Bounce, bump, and have a blast with your friends and family. It is a perfect way to add some excitement to your day!',
+    Rulebook: '',
+  },
+  {
+    id: 9,
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798002/INNO%20Fun%20Event/Club%20Events/Poster/vjbasjosguxlk77acgj0.jpg',
+    PreviewDescription: '',
+    Rulebook: '',
+  },
+  {
+    id: 10,
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798001/INNO%20Fun%20Event/Club%20Events/Poster/af35sfcdk1mqojxgxy8p.jpg',
+    PreviewDescription: '',
+    Rulebook: '',
+  },
+  {
+    id: 11,
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798001/INNO%20Fun%20Event/Club%20Events/Poster/tygzdcltb6g6fwqildqu.jpg',
+    PreviewDescription: '',
+    Rulebook: '',
+  },
+  {
+    id: 12,
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798001/INNO%20Fun%20Event/Club%20Events/Poster/ohdspicrj5sspziwtdao.jpg',
+    PreviewDescription: '',
+    Rulebook: '',
+  },
+  {
+    id: 13,
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798001/INNO%20Fun%20Event/Club%20Events/Poster/sjqnv5yxlpo9kgsilxag.jpg',
+    PreviewDescription: '',
+    Rulebook: '',
+  },
+  {
+    id: 14,
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798000/INNO%20Fun%20Event/Club%20Events/Poster/kvrzykiukdbxmjbw0m2r.jpg',
+    PreviewDescription: '',
+    Rulebook: '',
+  },
+  {
+    id: 15,
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798000/INNO%20Fun%20Event/Club%20Events/Poster/zcztvmxfwmw52uyd3txy.jpg',
     PreviewDescription: '',
     Rulebook: '',
   },
@@ -61,9 +117,9 @@ export const FunEvents_descriptionItems = [
   {
     id: 1,
     Heading: 'QUICK HANDS',
-    Description: '',
-    ImageURL:
-      'https://res.cloudinary.com/dhv234qct/image/upload/v1729520868/FunEvents/vfk7zxch1vxsdzdaibep.jpg',
+    Description:
+      'The Quick Hand Game is a thrilling test of speed and reflexes! Players dive into rapid-fire challenges grab in the blink of an eye. Whether for fun or focus, it’s guaranteed to keep you on your toes! Time: 9-5 ,Day : All three days',
+    ImageURL: 'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797486/Quick_Hand_aeeiun.jpg',
     Location: 'TBA',
     Date: 'TBA',
     Time: 'TBA',
@@ -72,9 +128,10 @@ export const FunEvents_descriptionItems = [
   {
     id: 2,
     Heading: 'GUESS THE CHARACTER',
-    Description: '',
+    Description:
+      'Can you crack the code? Test your detecƟve skills and try to idenƟfy the mystery character hidden inside the box. Are you ready to take on the challenge?',
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621431/INNO_events/Inno%20Club%20Posters/whxm03bgnue8zsarqtih.jpg',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797484/Guess_The_Character_hnrfn5.jpg',
     Location: 'TBA',
     Date: 'TBA',
     Time: 'TBA',
@@ -83,9 +140,10 @@ export const FunEvents_descriptionItems = [
   {
     id: 3,
     Heading: 'GEL-BLASTER COMBACT',
-    Description: '',
+    Description:
+      'Get ready for an epic baƩle! Grab your blaster and compete in intense, acƟon-packed matches. Show off your skills and emerge victorious in this thrilling event. Time: 9-5 Day : All three days',
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621429/INNO_events/Inno%20Club%20Posters/l9m6v9lkfioqht4d6mof.jpg',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797482/Gel_Blaster_bdckk8.png',
     Location: 'TBA',
     Date: 'TBA',
     Time: 'TBA',
@@ -94,9 +152,10 @@ export const FunEvents_descriptionItems = [
   {
     id: 4,
     Heading: 'BOX CRICKET',
-    Description: '',
+    Description:
+      'Are you ready to showcase your crickeƟng skills? Join us for an exciƟng box cricket tournament. Assemble your team and compete for the championship. Let the games begin!',
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621431/INNO_events/Inno%20Club%20Posters/gza47dud18pnjymfupjh.jpg',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797483/Box_Cricket_t2tvi5.jpg',
     Location: 'TBA',
     Date: 'TBA',
     Time: 'TBA',
@@ -105,9 +164,10 @@ export const FunEvents_descriptionItems = [
   {
     id: 5,
     Heading: 'SOFT ARCHERY',
-    Description: '',
+    Description:
+      'Unleash your inner archer at Innovision 2024! Test your aim and have fun with soŌ-Ɵpped arrows. Whether you are a seasoned pro or a beginner, this exciƟng event offers a unique experience for all.',
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621429/INNO_events/Inno%20Club%20Posters/dtcolsieyzwjkjpnmin4.jpg',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797486/Soft_Archery_zmfbve.jpg',
     Location: 'TBA',
     Date: 'TBA',
     Time: 'TBA',
@@ -116,9 +176,10 @@ export const FunEvents_descriptionItems = [
   {
     id: 6,
     Heading: 'BALLOON SHOOTING',
-    Description: '',
+    Description:
+      'Test your aim and accuracy in our fun-filled balloon shooƟng challenge. See how many balloons you can pop in a given Ɵme. Compete against friends and family and aim for the top spot!',
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621430/INNO_events/Inno%20Club%20Posters/l3re28rbhpqgj3z1qiqr.jpg',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797484/Balloon_Shooting_qlpxxp.jpg',
     Location: 'TBA',
     Date: 'TBA',
     Time: 'TBA',
@@ -127,9 +188,10 @@ export const FunEvents_descriptionItems = [
   {
     id: 7,
     Heading: 'CUP TIC-TAC-TOE',
-    Description: '',
+    Description:
+      'Calling all Tic-Tac-Toe enthusiasts! Join us for a thrilling tournament at the Innovision . Show off your strategic skills and compete against fellow players in this fun-filled event. Whether you are a seasonedpro or just starƟng out, everyone is welcome to participate.',
     ImageURL:
-      'https://res.cloudinary.com/duvkctvfr/image/upload/v1729621430/INNO_events/Inno%20Club%20Posters/hzbnmxl0ypfqgtxtr7va.jpg',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797484/Cup_Tic_Tac_Toe_vfa3l1.jpg',
     Location: 'TBA',
     Date: 'TBA',
     Time: 'TBA',
@@ -138,9 +200,87 @@ export const FunEvents_descriptionItems = [
   {
     id: 8,
     Heading: 'BODY ZORBING',
+    Description:
+      'Get ready for a thrilling adventure! Experience the joy of body zorbing, a fun and exciƟng acƟvity where you roll around in a giant inflatable ball. Bounce, bump, and have a blast with your friends and family. It is a perfect way to add some excitement to your day!',
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729797485/Body_Zorbing_podoll.jpg',
+    Location: 'TBA',
+    Date: 'TBA',
+    Time: 'TBA',
+    Prizes: 'TBA',
+  },
+  {
+    id: 9,
+    Heading: 'NITR GOT LATENT',
     Description: '',
     ImageURL:
-      'https://res.cloudinary.com/dhv234qct/image/upload/v1729520803/FunEvents/lozbhvbxshwllkcm9fyk.jpg',
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798002/INNO%20Fun%20Event/Club%20Events/Poster/vjbasjosguxlk77acgj0.jpg',
+    Location: 'TBA',
+    Date: 'TBA',
+    Time: 'TBA',
+    Prizes: 'TBA',
+  },
+  {
+    id: 10,
+    Heading: 'DRIFTATHON',
+    Description: '',
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798001/INNO%20Fun%20Event/Club%20Events/Poster/af35sfcdk1mqojxgxy8p.jpg',
+    Location: 'TBA',
+    Date: 'TBA',
+    Time: 'TBA',
+    Prizes: 'TBA',
+  },
+  {
+    id: 11,
+    Heading: 'ESCAPE ROOM 2.0',
+    Description: '',
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798001/INNO%20Fun%20Event/Club%20Events/Poster/tygzdcltb6g6fwqildqu.jpg',
+    Location: 'TBA',
+    Date: 'TBA',
+    Time: 'TBA',
+    Prizes: 'TBA',
+  },
+  {
+    id: 12,
+    Heading: 'THE AMAZING RACE',
+    Description: '',
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798001/INNO%20Fun%20Event/Club%20Events/Poster/ohdspicrj5sspziwtdao.jpg',
+    Location: 'TBA',
+    Date: 'TBA',
+    Time: 'TBA',
+    Prizes: 'TBA',
+  },
+  {
+    id: 13,
+    Heading: 'BRAINY ESCAPE 3.0',
+    Description: '',
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798001/INNO%20Fun%20Event/Club%20Events/Poster/sjqnv5yxlpo9kgsilxag.jpg',
+    Location: 'TBA',
+    Date: 'TBA',
+    Time: 'TBA',
+    Prizes: 'TBA',
+  },
+  {
+    id: 14,
+    Heading: 'FUN FUSION',
+    Description: '',
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798000/INNO%20Fun%20Event/Club%20Events/Poster/kvrzykiukdbxmjbw0m2r.jpg',
+    Location: 'TBA',
+    Date: 'TBA',
+    Time: 'TBA',
+    Prizes: 'TBA',
+  },
+  {
+    id: 15,
+    Heading: 'CYCLOTHON 3.0',
+    Description: '',
+    ImageURL:
+      'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729798000/INNO%20Fun%20Event/Club%20Events/Poster/zcztvmxfwmw52uyd3txy.jpg',
     Location: 'TBA',
     Date: 'TBA',
     Time: 'TBA',
