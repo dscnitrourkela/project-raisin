a814fb9bae4d732b11a9d717ab1ec7b75ebe1616
ayush <ayushatschool5@gmail.com>
2024-10-26 18:22:36 +0530

    chore: more fixes

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -1,6 +1,6 @@
-// lib/apollo-client.js
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import Cookies from 'js-cookie';
 
 const MAX_RETRIES = 3;
 const RETRY_TIMEOUT = 8000;
@@ -9,24 +9,31 @@ const reconnectFetch = async (uri, options, retries = 0) => {
   const controller = new AbortController();
   const timeoutId = setTimeout(() => controller.abort(), RETRY_TIMEOUT);
 
-  return fetch(uri, { ...options, signal: controller.signal })
-    .then((response) => {
-      clearTimeout(timeoutId);
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response;
-    })
-    .catch((error) => {
-      clearTimeout(timeoutId);
-      if (retries < MAX_RETRIES) {
-        console.log(`Retrying (${retries + 1}/${MAX_RETRIES})...`);
-        return new Promise((resolve) =>
-          setTimeout(() => resolve(customFetch(uri, options, retries + 1)), RETRY_TIMEOUT),
-        );
-      }
-      throw error;
+  try {
+    const response = await fetch(uri, {
+      ...options,
+      signal: controller.signal,
     });
+
+    clearTimeout(timeoutId);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return response;
+  } catch (error) {
+    clearTimeout(timeoutId);
+
+    if (retries < MAX_RETRIES) {
+      console.log(`Retrying (${retries + 1}/${MAX_RETRIES})...`);
+      return new Promise((resolve) =>
+        setTimeout(() => resolve(reconnectFetch(uri, options, retries + 1)), RETRY_TIMEOUT),
+      );
+    }
+
+    throw error;
+  }
 };
 
 const httpLink = new HttpLink({
@@ -36,14 +43,22 @@ const httpLink = new HttpLink({
 
 const authLink = setContext((_, { headers }) => {
   let token;
+
   if (typeof window !== 'undefined') {
-    token = localStorage.getItem('auth-token');
+    try {
+      const userDataFromCookie = Cookies.get('userData');
+      const parsedUserData = JSON.parse(userDataFromCookie);
+      token = parsedUserData.token;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error parsing userData from cookie');
+    }
   }
 
   return {
     headers: {
       ...headers,
-      Authorization: token ? `Bearer ${token}` : '',
+      Authorization: token || '',
     },
   };
 });
